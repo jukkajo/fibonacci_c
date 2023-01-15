0d60e5d78d6a16eb7f8ea010b96dcb580677428c
jii <76208694+jukkajo@users.noreply.github.com>
2023-01-15 04:12:53 +0200

    Update demo4vastaus.c

--- demo4vastaus.c
+++ demo4vastaus.c
@@ -1,134 +1,3 @@
-/**
- * Demo 4 kirjallinen palautustehtävä.
- *
- * Tämä koettaa johdatella käymään läpi samoja kysymyksiä, joita
- * ryhmäharjoitteessa käytiin läpi kasvotusten. Tee kaikki kohdat 1-5.
- *
- * 1. Tutustu seuraavaan C#-ohjelmaan:
- *
- *       https://replit.com/@JonneItkonen/QuerulousImpartialStructures
- *
- * (ryhmäharjoitteessa käydään läpi seuraavat kohdat keskustellen; älä
- * hajota keskittymistä ryhmätyössä niiden parissa, vaan tarvittaessa
- * voit kerrata _myöhemmin_; vain itsenäisesti tekevien tarvitsee
- * täyttää kohdat vastaukseksi demoon)
- *
- * 2. Kirjoita vastaukset seuraaviin kysymyksiin C#-esimerkkiin
- *    liittyen. Vastausten pohtiminen ennakkoon toivottavasti
- *    helpottaa myöhempien tehtävien tekemistä:
- *
- *    Kysymys: Millainen lukujono on Fibonacci? Miten se
- *    muodostetaan?
- *    Vastaus: Rekursiivisesti määriteltävä, haluttu luku muodostetaan
- *    summaamalla kaksi sitä edeltänyttä lukua. (1,2,3..55)
- *
- *    Kysymys: Mikä tietorakenne C#-esimerkkiohjelmassa on käytössä?
- *    Vastaus:  Kun esimerkkiohjelman kommenteissa sanotaan matkittaneen
- *    struct-tietuetta luokalla, niin ei liene väärin vastata rakenteeksi
- *    Luokan oliota? Muulloin se olisi struct.
- *
- *    Kysymys: Anna esimerkki rivistä, jolla varataan muistia pinosta.
- *    Vastaus: int e = lukujono.Arvo;
- *
- *    Kysymys: Anna esimerkki rivistä, jolla varataan muistia keosta.
- *    Vastaus: uusi = new Luku();
- *
- *    Kysymys: Anna esimerkki rivistä, jolla muutetaan pinomuistissa
- *    sijaitsevaa dataa.
- *    Vastaus: lkm = lkm - 2; //esimerkkikoodista
- *
- *    Kysymys: Anna esimerkki rivistä, jolla muutetaan kekomuistissa
- *    sijaitsevaa dataa.
- *    Vastaus: uusi.Arvo = arvo;
- *
- *    Kysymys: Milloin pinosta varattu muisti vapautuu?
- *    Vastaus: Kun funktion suoritus päättyy.
- *
- *    Kysymys: Milloin keosta varattu muisti vapautuu?
- *    Vastaus: Kun kutsutaan luokan tyhjennä-metodia. Kun C on kieli
- *    niin täytyy muisti vapauttaa itse ja näkisin sen tapahtuvan aikaisintaan
- *    tässä.
- *
- * 3. Tutustu tähän C-kieliseen esimerkkiohjelmaan ja vastaa
- *    seuraaviin kysymyksiin:
- *
- *    Kysymys: Mikä tietorakenne C-esimerkkiohjelmassa on käytössä?
- *    Vastaus: Tässäkin struct, tai sanotaan, tietue
- *
- *    Kysymys: Anna esimerkki rivistä, jolla varataan muistia pinosta.
- *    Vastaus: luku *lisaa(luku *lukujono, int arvo) 
- *
- *    Kysymys: Anna esimerkki rivistä, jolla varataan muistia keosta.
- *    Vastaus: uusi = malloc(sizeof(luku))
- *
- *    Kysymys: Anna esimerkki rivistä, jolla muutetaan pinomuistissa
- *    sijaitsevaa dataa.
- *    Vastaus: lkm = lkm - 2
- *
- *    Kysymys: Anna esimerkki rivistä, jolla muutetaan kekomuistissa
- *    sijaitsevaa dataa.
- *    Vastaus: uusi->arvo = arvo
- *
- *    Kysymys: Milloin pinosta varattu muisti vapautuu?
- *    Vastaus: Kun funktion suoritus loppuu tai se vapautetaan itse.
- *
- *    Kysymys: Milloin keosta varattu muisti vapautuu?
- *    Vastaus: Kun ohjelman suoritus päättyy tai se vapautetaan itse
- *    esim. free(jotakin);
- *
- *    Kysymys: Selitä, miksi aliohjelmaan lisaa() viedään osoitin
- *    luku-tietueeseen.
- *    Vastaus: '*'-merkki kuuluu paluutyyppiin, aliohjelma
- *    palauttaa siis osoittimen muistipaikkaan, jonka sisällön tyyppi ei ole tiedossa.
- *
- *    Kysymys: Miksi esimerkiksi rivillä
- *
- *                 lukujono = lisaa(lukujono, 3);
- *
- *    täytyy tehdä sijoitus muuttujaan lukujono? Ts. Miksi ei olisi
- *    vain
- *
- *                 lisaa(lukujono, 3);
- *
- *    Vastaus: Näin sijoitetaan arvot oikeaan muistipaikkaan.
- *
- *    Kysymys: Aliohjelman lisaa() kutsumuoto (engl. signature) on
- *
- *                luku *lisaa(luku *lukujono, int arvo);
- *
- *    Miksi se ei voisi olla
- *
- *                luku lisaa(luku *lukujono, int arvo);
- *
- *    Vastaus: Koska jatkoa ajatellen täytyy tietää muistipaikka jonka
- *    funktio '*'-merkillä palauttaa, ilman palauttaisin integerin    
- *
- * 4. Tallenna tämä koodi nimelle "demo4vastaus.c" ja muokkaa tätä
- *    niin, että tämä toimii samoin kuin kohdan 1 C#-ohjelma. Voit
- *    katsoa mallia siitä sekä alkuperäisestä esimerkkikoodista.
- *
- *    HUOM 1: Sinun tulee siis muokata tätä niin, että tämä laskee
- *    Fibonaccin lukujonon ja tallentaa sen samanlaiseen
- *    tietorakenteeseen kuin mitä C#-esimerkki käyttää!
- *
- *    HUOM 2: Helpointa lienee havaita, mikä on erilaista C:llä
- *    verrattuna C#-kieleen, ja sitten kääntää mallia C-kieliseksi
- *    rivi riviltä. Lopullinen vastaus voi lopulta hyvinkin sisältä
- *    täysin saman määrän ei-kommenttirivejä kuin C#-malli. Jokainen
- *    rivi voi tehdä olennaisesti saman asian C:llä kuin mitä C#:lla
- *    tehdään.
- *
- *    HUOM 3: Kuten aina, tee pieni muutos kerrallaan ja varmistu
- *    muutoksen jälkeen, että koodi edelleen kääntyy ja toimii
- *    nykyisen oletuksen mukaisesti. Sillä tavoin ei pääse kertymään
- *    montaa uutta virhettä kerralla, jolloin korjaaminen pysyy
- *    mahdollisena!
- *
- * 5. Palauta syntynyt tiedosto "demo4vastaus.c" kommentteineen.
- *
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -153,14 +22,6 @@ void tyhjenna(luku *lukujono) {
     free(lukujono);
     tyhjenna(seuraava);
 }
-
-/*
-int laske_summa(luku *lukujono, int summa) {
-    luku *nykyinen = lukujono;
-    if (nykyinen == NULL) return summa;
-    return laske_summa(nykyinen->seuraava, summa+nykyinen->arvo);
-}
-*/
 
 void tulostaLuvut(luku *lukujono, int mones) {
   luku *nykyinen = lukujono;
@@ -188,10 +49,6 @@ int main(int argc, char **argv) {
 
     luku *lukujono = lisaa(NULL, 1);
     lukujono = lisaa(lukujono, 1);
-
-    // Tässä lasket sitten C#-esimerkin mukaisesti
-    // Fibonaccin luvut. Nyt on vain kovakoodattu esimerkki
-    // ja lukujen summan laskeminen:
  
     for(int i=0; i < lkm; ++i) {
       int ed1 = lukujono->arvo;
@@ -201,12 +58,6 @@ int main(int argc, char **argv) {
     }
     int mones =lkm + 2;
     tulostaLuvut(lukujono, mones);
-
-    /*
-    int summa = laske_summa(lukujono, 0);
-    printf("summa on %d\n", summa);
-
-    */
 
     tyhjenna(lukujono);
 
